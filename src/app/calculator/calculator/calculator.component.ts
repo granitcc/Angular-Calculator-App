5001d16779ee2fb0c7d30c425c752396088a416f
granittc <66574493+granitcc@users.noreply.github.com>
2022-07-14 11:45:51 +0200

    Upload angular calculator app

---
+++ src/app/calculator/calculator/calculator.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-calculator",
+  templateUrl: "./calculator.component.html",
+  styleUrls: ["./calculator.component.scss"],
+})
+export class CalculatorComponent implements OnInit {
+  public currentNumber = "0";
+  private firstNumber: any = null;
+  private operator: any = null;
+  private waitForSecondNumber = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  getNumber(value: string) {
+    console.log(value);
+    if (this.waitForSecondNumber) {
+      this.currentNumber = value;
+      this.waitForSecondNumber = false;
+    } else {
+      this.currentNumber === "0"
+        ? this.currentNumber = value
+        : this.currentNumber += value;
+    }
+  }
+
+  getDecimal() {
+    if (!this.currentNumber.includes(".")) {
+      this.currentNumber += ".";
+    }
+  }
+
+  private doCalculation(operator: string, secondNumber: number) {
+    switch (operator) {
+      case "+":
+        return (this.firstNumber += secondNumber);
+      case "-":
+        return (this.firstNumber -= secondNumber);
+      case "x":
+        return (this.firstNumber *= secondNumber);
+      case "/":
+        return (this.firstNumber /= secondNumber);
+      case "=":
+        return secondNumber;
+    }
+  }
+
+  getOperation(operator: string) {
+    if (this.firstNumber === null) {
+      this.firstNumber = Number(this.currentNumber);
+    } else if (this.operator) {
+      const result = this.doCalculation(this.operator, Number(this.currentNumber));
+      this.currentNumber = String(result);
+      this.firstNumber = result;
+    }
+    this.operator = operator;
+    this.waitForSecondNumber = true;
+  }
+
+  cleanUp() {
+    this.currentNumber = "0";
+    this.firstNumber = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
+}
